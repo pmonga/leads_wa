6972b7e0c222babe5cd9fa4a4f5f6a16df8b7d6f
PM <prashant@onlinemocks.com>
2024-12-05 09:24:25 +0530

    WIP

--- helpers/config.js
+++ helpers/config.js
@@ -6,9 +6,4 @@ const VERSION = "1.0.0";
 const MAX_USERS = 1000;
 const FLOW_KBM = "1214667192982073";
 // Export individually
-export default {
-  APP_NAME,
-  VERSION,
-  MAX_USERS,
-  FLOW_KBM
-};
+export { APP_NAME, VERSION, MAX_USERS, FLOW_KBM };
