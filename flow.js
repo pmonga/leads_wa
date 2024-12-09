01876081abe9105650f307329352378d1733237d
PM <prashant@onlinemocks.com>
2024-12-09 15:18:19 +0530

    WIP

--- flow.js
+++ flow.js
@@ -35,23 +35,14 @@ export const getNextScreen = async (req, res, decryptedBody) => {
     };
   }
   const flow_obj = await get(flow_token);
-  const flow_id = flow_obj?.flow_token;
+  const flow_id = flow_obj?.flow_id;
   let response;
-  console.log("flow_object: ", flow_obj);
-  console.log(
-    "compare FLOW_KBM to flow_id: ",
-    FLOW_KBM,
-    flow_id == FLOW_KBM,
-    flow_id
-  );
   switch (flow_id) {
     // Flow KBM_test id 1214667192982073
     case FLOW_KBM:
-      console.log("Inside KBM");
       response = await KBMgetNextSCreen(req, res, decryptedBody);
       break;
     default:
-      console.log("Inside default");
       // close the flow;
       response = {
         screen: "SUCCESS",
