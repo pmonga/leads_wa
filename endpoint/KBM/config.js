477d7cc0313293c9d8ff64cc7669b221653d3d7d
PM <prashant@onlinemocks.com>
2024-12-16 10:40:53 +0530

    WIP- base_code added to campaigns

--- endpoint/KBM/config.js
+++ endpoint/KBM/config.js
@@ -1,12 +1,12 @@
 // KBN Game config.js
 
 // Constants and settings
-const APP_NAME = 'Kaun Banega MBA';
-const VERSION = '1.0.0';
-const GAME_TIME = [15 * 60 * 1000, 10 * 60 * 1000]; // 15 minutes in milliseconds
+const APP_NAME = "Kaun Banega MBA";
+const VERSION = "1.0.0";
+const GAME_TIME = [10 * 60 * 1000, 8 * 60 * 1000]; // 10 minutes in milliseconds
 const GAME_PRIZE = [
-  [1, 2, 4, 7, 10, 15, 20, 30, 50, 100],
-  [10, 20, 40, 70, 100, 150, 200, 300, 500, 1000],
+  [1, 2, 4, 7, 10, 15, 20, 25, 50, 100],
+  [10, 20, 40, 70, 100, 150, 200, 300, 500, 1000]
 ];
 
 // Export individually
--- webhook/messageTypeHandlers/handleTextMessage.js
+++ webhook/messageTypeHandlers/handleTextMessage.js
@@ -1,7 +1,7 @@
-import dotnenv from 'dotenv';
-import handleTEST01Campaign from '../campaignHandlers/handleTEST01Campaign.js';
-import handleXCD09GCampaign from '../campaignHandlers/handleXCD09GCampaign.js';
-import handleDefaultCampaign from '../campaignHandlers/handleDefaultCampaign.js';
+import dotnenv from "dotenv";
+import handleTEST01Campaign from "../campaignHandlers/handleTEST01Campaign.js";
+import handleXCD09GCampaign from "../campaignHandlers/handleXCD09GCampaign.js";
+import handleDefaultCampaign from "../campaignHandlers/handleDefaultCampaign.js";
 
 dotnenv.config();
 
@@ -16,29 +16,31 @@ export default async (req, res, next) => {
   const campaignRegex = /^\[([A-Za-z0-9]{6})\]/;
   const match = message.text.body.match(campaignRegex);
   const code = match && campaigns[match[1]] ? match[1] : false;
+  let base_code, campaign;
 
   if (code) {
     res.locals.code = code;
-    res.locals.campaign = campaigns[code];
+    res.locals.campaign = campaign = campaigns[code];
+    base_code = campaigns[code]?.base_code || code;
     const utm = res.locals.campaign?.utm ? { ...res.locals.campaign.utm } : {};
     res.locals.crm.utm = { ...res.locals.crm.utm, ...utm };
-    res.locals.crm.message += ' ' + message.text.body;
+    res.locals.crm.message += " " + message.text.body;
     contact.createdBy = contact.createdBy || code;
     contact.tagsToAdd = Array.isArray(campaigns[code]?.tags)
-      ? [code, ...campaigns[code]?.tags]
+      ? [code, ...campaigns[code].tags]
       : [code];
   } else {
-    res.locals.crm.utm = { ...res.locals.crm.utm, utm_source: 'self' };
-    res.locals.crm.message += ' ' + message.text.body;
-    contact.tagsToAdd = [...contact.tagsToAdd, 'self'];
-    contact.createdBy = contact.createdBy || 'self';
+    res.locals.crm.utm = { ...res.locals.crm.utm, utm_source: "self" };
+    res.locals.crm.message += " " + message.text.body;
+    contact.tagsToAdd = [...contact.tagsToAdd, "self"];
+    contact.createdBy = contact.createdBy || "self";
   }
 
-  switch (code) {
-    case 'TEST01':
+  switch (base_code) {
+    case "TEST01":
       await handleTEST01Campaign(req, res);
       break;
-    case 'XCD09G':
+    case "XCD09G":
       await handleXCD09GCampaign(req, res);
       break;
     default:
