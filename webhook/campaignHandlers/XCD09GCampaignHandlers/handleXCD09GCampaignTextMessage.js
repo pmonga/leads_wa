ab8ae226f0c2406160a5dfa1ed8187b748d9988c
PM <prashant@onlinemocks.com>
2024-12-29 13:19:03 +0530

    WIP

--- webhook/campaignHandlers/XCD09GCampaignHandlers/handleXCD09GCampaignTextMessage.js
+++ webhook/campaignHandlers/XCD09GCampaignHandlers/handleXCD09GCampaignTextMessage.js
@@ -11,7 +11,7 @@ import {
   sendKBMFlow,
   sendSignUpFlow,
   sendRegistrationMessage
-} from "./XCD09GCHandlerFunctions.js";
+} from "./handlerFunctions.js";
 
 dotnenv.config();
 export default async (req, res, next) => {
