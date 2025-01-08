093419413b052bdd8405e6da61d23d7a376b198a
PM <prashant@onlinemocks.com>
2025-01-08 23:09:51 +0530

    WIP

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.get("/kbm", (req, res) => {
 });
 
 app.get("/sendkbmReminder", async (req, res) => {
-  if (!isInTimeRange("10:00", "10:01")) {
+  if (!isInTimeRange("23:00", "23:15")) {
     console.log("out of range");
     return res.status(403).send("Not in allowed time range");
   }
