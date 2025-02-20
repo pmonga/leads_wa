27e2cbfa61f92396a41b51eeb55fc6c6f3fb7bd9
PM <prashant@onlinemocks.com>
2025-02-20 12:01:51 +0530

    TnC updated for cash payout

--- endpoint/KBM/config.js
+++ endpoint/KBM/config.js
@@ -29,13 +29,13 @@ const TERMS = `## 1. Eligibility
 
 ## 2. Gameplay Rules
 2.1. Each player can participate thrice per day. More than 3 attempts per day or registrations under different email IDs or phones (including those of friends and family) will result in immediate disqualification.
-2.2. Players can only leave the game between questions, not during an ongoing question. Once a question is started, the player must answer it.
+2.2. Players can quit the game during a question by selectiong quit option but will need to provide an answer to it. There will be no reward/ penalty for it but feedback will be provided.
 2.3. Players must solve each question in the given time. There is no independent or additional time provided for any question.
 2.4. An incorrect answer at any stage will result in forfeiture of all accumulated rewards. No appeals or reviews will be allowed.
 2.5. The decision to leave the game must be explicitly confirmed by the player within the game interface.
 
 ## 3. Rewards
-3.1. Rewards are non-negotiable and non-transferable. The participant’s details, including the registered name, email ID, and phone number, must exactly match the name and details provided in the recipient’s bank account or payment method. Students not registered at Alchemist for comprehensive coaching for CAT 25 or CAT 26 may not claim cash rewards more than rupees fifty.
+3.1. Rewards are non-negotiable and non-transferable. The participant’s details, including the registered name, email ID, and phone number, must exactly match the name and details provided in the recipient’s bank account or payment method. Students not registered at Alchemist for comprehensive coaching for CAT 25 or CAT 26 may not claim cash rewards.
 3.2. Players must claim rewards by sending an email to the designated address from their registered email ID.
 3.3. Any failure to follow the claim process, including mismatched details, incomplete information, or delayed submissions, will result in forfeiture of the reward.
 3.4. Rewards will be processed within 7 working days, but Alchemist and its associates also referred to as the institute in this document reserves the right to delay or cancel payment due to suspected fraud or non-compliance with these terms.
