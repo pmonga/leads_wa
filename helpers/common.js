650205fbf99a66e224220abac81b29bb0294096f
PM <prashant@onlinemocks.com>
2025-07-03 10:19:11 +0530

    Broardcast youtube preview working

--- helpers/common.js
+++ helpers/common.js
@@ -15,7 +15,7 @@ const broadcast = async function (message, waClient, contactsCollection) {
   let promises = [];
   contacts.forEach((e) => {
     promises = promises.concat(
-      waClient.sendTextMessage(/*e.phone*/ "9136465050", {
+      waClient.sendTextMessage(e.phone, {
         preview_url: true,
         body: interpolateString(message, e)
       })
